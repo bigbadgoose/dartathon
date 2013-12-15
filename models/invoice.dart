bd89dac51d62f0b113aa1160a675b579242ea3be
douglee <doug@bigbadgoose.com>
2013-12-14 18:44:08 -0800

    styling pass 3

--- models/invoice.dart
+++ models/invoice.dart
@@ -8,7 +8,8 @@ class Invoice {
   List<LineItem> lineItems = [];
   
   Invoice(this.id) {
-    this.lineItems.add(new LineItem('Guidevine', 'Desc', [new Charge('Heroku', 84.0, 'dollar'), new Charge('Sales Tax', 10.0, 'percent')], new DateTime.now()));
-    this.lineItems.add(new LineItem('Topdown', 'Desc', [new Charge('Feature 1', 2000.0, 'dollar')], new DateTime.now()));
+    this.lineItems.add(new LineItem('Guidevine Monthly Maintenance', 'Desc', [new Charge('Heroku', 84.0, 'dollar'), new Charge('Sales Tax', 10.0, 'percent')], new DateTime.now()));
+    this.lineItems.add(new LineItem('Development: Anonymous Survey Feature', 'Desc', [new Charge('Feature 1', 2000.0, 'dollar')], new DateTime.now()));
+    this.lineItems.add(new LineItem('Development: New Experience Design', 'Desc', [new Charge('Feature 1', 4000.0, 'dollar')], new DateTime.now()));
   }
 }
