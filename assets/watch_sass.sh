a21d7b2945a042076beae072ab798482f324e175
douglee <doug@bigbadgoose.com>
2013-12-14 16:13:15 -0800

    added sass

---
+++ assets/watch_sass.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+#bundle exec sass --style expanded --watch sass/style.scss:style.css
+#bundle exec sass --watch --debug-info sass/style.scss:style.css
+
+bundle exec sass --watch sass/style.scss:css/style.css
